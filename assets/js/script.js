298af01841e3d5467f50ea03d627a29cf30d58fc
lills1 <lilly.stephenson4@gmail.com>
2021-10-02 17:32:20 +1000

    commit to my local before pull'

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,18 @@
 let mapBoxAPIKey = config.mapBoxAPIKey;
+var video = document.getElementById("myVideo");
+
 
 var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
 L.tileLayer('https://api.mapbox.com/styles/v1.7.1/mapid/tiles/0/0/0?access_token=' + mapBoxAPIKey, {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    // mapbox://styles/mapbox/navigation-day-v1
-    // styles/v1.7.1/mapid/tiles/{z}/{x}/{y}
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: mapBoxAPIKey,
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  // mapbox://styles/mapbox/navigation-day-v1
+  // styles/v1.7.1/mapid/tiles/{z}/{x}/{y}
+  id: 'mapbox/streets-v11',
+  tileSize: 512,
+  zoomOffset: -1,
+  accessToken: mapBoxAPIKey,
 }).addTo(mymap);
 
 var marker = L.marker([51.5, -0.09]).addTo(mymap);
@@ -21,3 +23,32 @@ var circle = L.circle([51.508, -0.11], {
   fillOpacity: 0.5,
   radius: 500
 }).addTo(mymap);
+
+
+fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/?query=Stockholm", {
+  "method": "GET",
+  "headers": {
+    "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+    "x-rapidapi-key": "2674f7eae4mshe1b8c818d721bd4p1a7642jsnf211bec92621"
+  }
+})
+  .then(response => {
+    console.log(response);
+  })
+  .catch(err => {
+    console.error(err);
+  });
+
+// Get the button
+var btn = document.getElementById("myBtn");
+
+// Pause and play the video, and change the button text
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
